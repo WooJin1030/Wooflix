22d11a79f1a39f74a514a20cfe509bbbd7c7f245
Woojin <sam4woojin@naver.com>
2021-06-18 16:21:48 +0900

    Loader & TVPresenter

--- src/Routes/TV/TVPresenter.js
+++ src/Routes/TV/TVPresenter.js
@@ -1,9 +1,41 @@
 import React from "react";
 import PropTypes from "prop-types";
 import styled from "styled-components";
+import Section from "Components/Section";
+import Loader from "Components/Loader";
+
+const Container = styled.div`
+  padding: 0px 10px;
+`;
 
 const TVPresenter = ({ topRated, popular, airingToday, error, loading }) =>
-  null;
+  loading ? (
+    <Loader />
+  ) : (
+    <Container>
+      {topRated && topRated.length > 0 && (
+        <Section title="Top Rated Shows">
+          {topRated.map((movie) => (
+            <span key={movie.id}>{movie.name}</span>
+          ))}
+        </Section>
+      )}
+      {popular && popular.length > 0 && (
+        <Section title="Popular Shows">
+          {popular.map((movie) => (
+            <span key={movie.id}>{movie.name}</span>
+          ))}
+        </Section>
+      )}
+      {airingToday && airingToday.length > 0 && (
+        <Section title="Airing Today">
+          {airingToday.map((movie) => (
+            <span key={movie.id}>{movie.name}</span>
+          ))}
+        </Section>
+      )}
+    </Container>
+  );
 
 TVPresenter.propTypes = {
   topRated: PropTypes.array,
