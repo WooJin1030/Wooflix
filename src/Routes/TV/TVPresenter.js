aa0846d9eb2fdfafe6d88c625f5d4cba70e90b6e
Woojin <sam4woojin@naver.com>
2021-06-24 21:06:46 +0900

    React Helmet Title

--- src/Routes/TV/TVPresenter.js
+++ src/Routes/TV/TVPresenter.js
@@ -1,6 +1,7 @@
 import React from "react";
 import PropTypes from "prop-types";
 import styled from "styled-components";
+import Helmet from "react-helmet";
 import Section from "Components/Section";
 import Loader from "Components/Loader";
 import Message from "Components/Message";
@@ -10,59 +11,65 @@ const Container = styled.div`
   padding: 20px;
 `;
 
-const TVPresenter = ({ topRated, popular, airingToday, error, loading }) =>
-  loading ? (
-    <Loader />
-  ) : (
-    <Container>
-      {topRated && topRated.length > 0 && (
-        <Section title="Top Rated Shows">
-          {topRated.map((tv) => (
-            <Poster
-              key={tv.id}
-              id={tv.id}
-              title={tv.original_name}
-              imageUrl={tv.poster_path}
-              rating={tv.vote_average}
-              isMovie={false}
-              year={tv.first_air_date && tv.first_air_date.substring(0, 4)}
-            />
-          ))}
-        </Section>
-      )}
-      {popular && popular.length > 0 && (
-        <Section title="Popular Shows">
-          {popular.map((tv) => (
-            <Poster
-              key={tv.id}
-              id={tv.id}
-              title={tv.original_name}
-              imageUrl={tv.poster_path}
-              rating={tv.vote_average}
-              isMovie={false}
-              year={tv.first_air_date && tv.first_air_date.substring(0, 4)}
-            />
-          ))}
-        </Section>
-      )}
-      {airingToday && airingToday.length > 0 && (
-        <Section title="Airing Today">
-          {airingToday.map((tv) => (
-            <Poster
-              key={tv.id}
-              id={tv.id}
-              title={tv.original_name}
-              imageUrl={tv.poster_path}
-              rating={tv.vote_average}
-              isMovie={false}
-              year={tv.first_air_date && tv.first_air_date.substring(0, 4)}
-            />
-          ))}
-        </Section>
-      )}
-      {error && <Message color="#e74c3c" text={error} />}
-    </Container>
-  );
+const TVPresenter = ({ topRated, popular, airingToday, error, loading }) => (
+  <>
+    <Helmet>
+      <title>TV Shows | Wooflix</title>
+    </Helmet>
+    {loading ? (
+      <Loader />
+    ) : (
+      <Container>
+        {topRated && topRated.length > 0 && (
+          <Section title="Top Rated Shows">
+            {topRated.map((tv) => (
+              <Poster
+                key={tv.id}
+                id={tv.id}
+                title={tv.original_name}
+                imageUrl={tv.poster_path}
+                rating={tv.vote_average}
+                isMovie={false}
+                year={tv.first_air_date && tv.first_air_date.substring(0, 4)}
+              />
+            ))}
+          </Section>
+        )}
+        {popular && popular.length > 0 && (
+          <Section title="Popular Shows">
+            {popular.map((tv) => (
+              <Poster
+                key={tv.id}
+                id={tv.id}
+                title={tv.original_name}
+                imageUrl={tv.poster_path}
+                rating={tv.vote_average}
+                isMovie={false}
+                year={tv.first_air_date && tv.first_air_date.substring(0, 4)}
+              />
+            ))}
+          </Section>
+        )}
+        {airingToday && airingToday.length > 0 && (
+          <Section title="Airing Today">
+            {airingToday.map((tv) => (
+              <Poster
+                key={tv.id}
+                id={tv.id}
+                title={tv.original_name}
+                imageUrl={tv.poster_path}
+                rating={tv.vote_average}
+                isMovie={false}
+                year={tv.first_air_date && tv.first_air_date.substring(0, 4)}
+              />
+            ))}
+          </Section>
+        )}
+        {error && <Message color="#e74c3c" text={error} />}
+      </Container>
+    )}
+  </>
+);
 
 TVPresenter.propTypes = {
   topRated: PropTypes.array,
